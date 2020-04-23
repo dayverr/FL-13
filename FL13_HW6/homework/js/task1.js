67746ff8748f85789a67debfef5a76946b24fc7a
Viktor Marchuk <dayverr87@gmail.com>
2020-04-23 23:32:56 +0300

    Add files via upload

---
+++ FL13_HW6/homework/js/task1.js
@@ -0,0 +1,15 @@
+let check = prompt('Input your check');
+if (isNaN(check)===false && check>0) {
+    check=Number(check);
+    let tip = +prompt('Input percentage of tip');
+    if (isNaN(tip)===false && tip>0 && tip<=100) {
+        tip=Number(tip);
+        let tipAm = check*tip/100;
+        let total = check+tipAm;
+        alert('Check number: '+check+'\nTip: '+tip+'%'+'\nTip amount: '+tipAm+'\nTotal sum to pay: '+total);
+    } else {
+        alert('Wrong tip input');
+    }
+} else {
+    alert('Wrong  check input');
+}
---
+++ FL13_HW6/homework/js/task2.js
@@ -0,0 +1,14 @@
+let word = prompt('Input your word');
+if (/\s/.test(word)||word==='') {
+    alert('wrong input');
+} else {
+    let arr = Array.from(word);
+    let i = arr.length;
+    if (i===1) {
+    alert(arr[0]);
+    } else if (i%2===0){
+    alert(arr[i/2-1] + arr[i/2]);
+    } else if (i%2){
+    alert(arr[(i-1)/2]);
+    }
+}
