3bf5407d8f4cdcfa60b3f7e64f4cb4d534862558
Viktor Marchuk <dayverr87@gmail.com>
2020-04-28 20:29:58 +0300

    HW7:ADD: task1

--- FL13_HW7/homework/src/js/task1.js
+++ FL13_HW7/homework/src/js/task1.js
@@ -1,1 +1,46 @@
-// Your code goes here
+function checkLogin(name){
+    if (name===''){
+        return alert('Canceled');
+    } else if (Array.from(name).length<4) {
+        return alert('I dont know any users having name length less than 4 symbols');
+    } else if (name==='User'||name==='Admin'){
+        return 'valid';
+    } else {
+        return alert('I don’t know you');
+    }
+}
+
+function checkPass(pass,login){
+    if (pass===''){
+        return alert('Canceled');
+    } else if (login==='User'&&pass==='UserPass') {
+        return 'valid';
+    } else if (login==='Admin'&&pass==='RootPass'){
+        return 'valid';
+    } else {
+        return alert('Wrong password');
+    }
+}
+
+
+
+function greet(login){
+    let curentTime = new Date().getHours();
+    if(curentTime<20) {
+        alert('Good day, dear '+login);
+    } else {
+        alert('Good evening, dear '+login);
+    }
+}
+
+
+
+
+let login = prompt('Input login :');
+
+if (checkLogin(login)==='valid'){
+    let password = prompt('input password :');
+    if (checkPass(password,login)==='valid'){
+        greet(login);
+    }
+}
