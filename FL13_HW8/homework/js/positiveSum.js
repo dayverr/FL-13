7c202f00c836fffa1966d6a7268c33f83c61a259
Viktor Marchuk <dayverr87@gmail.com>
2020-04-30 23:21:44 +0300

    HW8:ADD: positivesum

---
+++ FL13_HW8/homework/js/positiveSum.js
@@ -0,0 +1,11 @@
+function posiitiveSum (arr){
+    let result=0;
+    for (let i = 0; i<arr.length; i++){
+        if(arr[i]>0) {
+            result+=arr[i];
+        }
+    }
+    return result;
+}
+
+posiitiveSum()
