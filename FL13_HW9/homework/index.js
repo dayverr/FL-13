c74e1dcc88de56a58b4fad614ee16f3e92303359
Viktor Marchuk <dayverr87@gmail.com>
2020-05-05 21:48:19 +0300

    HW9: ADD func

--- FL13_HW9/homework/index.js
+++ FL13_HW9/homework/index.js
@@ -1,1 +1,23 @@
-// Your code goes here
+function convert () {
+    let arr=[];
+    for (let i=0; i<arguments.length; i++){
+        typeof arguments[i]==='string' ? arr[i]=Number(arguments[i]) : arr[i]=String(arguments[i]);
+    }
+    return arr;
+}
+
+
+function executeforEach (arr,func){
+    for (let i=0;i<arr.length;i++){
+        arr[i]=func(arr[i]);
+    }
+    return arr;
+}
+
+
+function mapArray(arr,func) {
+    executeforEach(arr,function(element) {
+ return Number(element)
+});
+    return executeforEach(arr,func);
+}
