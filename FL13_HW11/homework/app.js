dd5e0d53b0460c575d0d8fd7f09581bf05331afc
Viktor Marchuk <dayverr87@gmail.com>
2020-05-12 23:57:55 +0300

    ADD:features

--- FL13_HW11/homework/app.js
+++ FL13_HW11/homework/app.js
@@ -52,4 +52,56 @@ const data = [
 
 const rootNode = document.getElementById('root');
 
-// TODO: your code goes here
+function createTree(container, arr) {
+  container.append(createTreeDom(arr));
+}
+
+function createTreeDom(arr) {
+  if (!arr.length) {
+ return;
+}
+  let ul = document.createElement('ul');
+  for (let i=0; i<arr.length; i++) {
+    let li = document.createElement('li');
+    li.innerHTML = `<i class="material-icons folder">folder_open</i><span> ${arr[i]['title']}</span> `;
+    if(!arr[i]['folder']) {
+      li.innerHTML = `<i class="material-icons file">insert_drive_file</i><span> ${arr[i]['title']}</span> `;
+    }
+    if(arr[i]['folder']&&arr[i]['children']===null){
+      li.innerHTML =`<i class="material-icons folder">folder</i><span class="noChildren"> ${arr[i]['title']}</span> `;
+    }
+      if (arr[i]['children']!==null){
+      let childrenUl = createTreeDom(arr[i]['children']);
+      if (childrenUl) {
+        li.append(childrenUl);
+      }
+    }
+    ul.append(li);
+  }
+  return ul;
+}
+
+let container = document.getElementById('root');
+createTree(container, data);
+
+container.onclick = function(event) {
+
+  if (event.target.tagName !== 'SPAN') {
+    return;
+  }
+  let childrenContainer = event.target.parentNode.querySelector('ul');
+  if (!childrenContainer) {
+ return;
+}
+  if (event.target.previousSibling.innerHTML === 'folder_open'){
+    event.target.previousSibling.innerHTML = 'folder';
+  } else if (event.target.previousSibling.innerHTML === 'folder'){
+    event.target.previousSibling.innerHTML = 'folder_open';
+  }
+
+  if (event.target.classList.contains('noChildren')){
+    event.target.parentNode.append('ul')
+  }
+
+  childrenContainer.hidden = !childrenContainer.hidden;
+}
