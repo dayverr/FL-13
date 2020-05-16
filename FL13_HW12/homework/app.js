42588c1c42b3a750d8dc1ebe1b01cb5022aeeb4b
Viktor Marchuk <dayverr87@gmail.com>
2020-05-16 19:33:23 +0300

    ADD: features

--- FL13_HW12/homework/app.js
+++ FL13_HW12/homework/app.js
@@ -1,1 +1,38 @@
 const root = document.getElementById('root');
+
+let lockalBooks = JSON.parse(localStorage.getItem('books'));
+let div = document.createElement('div');
+
+div.classList.add('left-wrapper')
+div.innerHTML='<h2>List of books</h2>';
+root.append(div);
+
+let div2 = document.createElement('div');
+div2.classList.add('right-wrapper');
+div2.innerHTML='<h2>Book information</h2>';
+root.append(div2);
+
+let list = document.querySelector('.left-wrapper');
+let list2 = document.querySelector('.right-wrapper');
+
+lockalBooks.forEach(element => {
+    let temp = `<div><a href="#${element['id']}">${element['name']}</a><button сlass="edit">edit</button></div>`;
+    list.insertAdjacentHTML('beforeend', temp);
+});
+
+let temp= `<p>Book : ${lockalBooks[0]['name']}</p><p>Author : ${lockalBooks[0]['author']}</p>`
+list2.innerHTML = temp + `<img src="${lockalBooks[0]['img']}"><p>Plot : ${lockalBooks[0]['plot']}</p>`;
+
+list.insertAdjacentHTML('beforeend','<button>add</button>');
+
+function dinamickContent() {
+    let contentId = location.hash.slice(1);
+    lockalBooks.forEach(element => {
+        if(element['id']===contentId){
+            let temp = `<p>Book : ${element['name']}</p><p>Author : ${element['author']}</p>`;
+            list2.innerHTML = temp + `<img src="${element['img']}"><p>Plot : ${element['plot']}</p>`;
+        }
+    })
+}
+
+window.addEventListener('hashchange', dinamickContent);
---
+++ FL13_HW12/homework/books.js
@@ -0,0 +1,25 @@
+let books = [
+    {
+        'id': '123',
+        'name':'fallout',
+        'author': 'ivanov',
+        'img':'https://www.bookclub.ua/images/db/goods/k/53454_98763_k.jpg',
+        'plot': `some text some text some text some text some text some text some text `
+    },
+    {
+        'id': '124',
+        'name':'space rangers',
+        'author': 'petrov',
+        'img':'https://www.bookclub.ua/images/db/goods/k/53471_98145_k.jpg',
+        'plot': `some text some text some text some text some text some text some text `
+    },
+    {
+        'id': '125',
+        'name':'heroes of might and magic III',
+        'author': 'sidorov',
+        'img':'https://www.bookclub.ua/images/db/goods/k/35333_52676_k.jpg',
+        'plot': `some text some text some text some text some text some text some text `
+    }
+]
+
+localStorage.setItem('books', JSON.stringify(books));
