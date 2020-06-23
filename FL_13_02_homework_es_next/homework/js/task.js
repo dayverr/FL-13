31a6ed9db1c50f0747a8cb81bf0a5b119c903427
Viktor Marchuk <dayverr87@gmail.com>
2020-06-23 22:33:40 +0300

    ADD 3

--- FL_13_02_homework_es_next/homework/js/task.js
+++ FL_13_02_homework_es_next/homework/js/task.js
@@ -1,3 +1,9 @@
 const maxElement = (arr) => Math.max.apply(null, arr);
 const copiedArray = (arr) => [...arr];
 
+function addUniqueId (obj){
+    let copyObj=Object.assign({}, obj);
+    let id = Symbol("id");
+    copyObj[id]=1;
+    return copyObj;
+}
