df27e88f78c886ad45b2dd40e29a6fdb743b16e1
Viktor Marchuk <dayverr87@gmail.com>
2020-06-26 00:02:51 +0300

    ADD: HW3

---
+++ FL13_Homework_3_jquery/task1/task1/js/index.js
@@ -0,0 +1,43 @@
+// Example: $('#someId').css({'prop' : 'value', 'prop1' : 'value1', ...});
+
+//body: add padding 30px
+
+// id title: make align center, remove top margin
+
+// div after header: add double border and padding 20px
+
+// change color for all h2 and set top margin to 0
+
+// set font size 18px for all .list direct children
+
+
+// in #list-1
+  // show all hidden and not cloned li
+
+  // hide empty li
+
+// in #list-3
+  // for all even li set margin-left -20px
+
+  // for the first li set any different color
+
+  // for all li with index > 5 set color to #ccc
+
+// for li wich has em add red color
+
+// for li which contains text 'Buratino' set font weight to bold
+
+
+// for b in p which is the only child set font size 36px
+
+// for em in p which is the last child of type set color to green
+
+
+// set width 80px for input with attribute name ended by 'age'
+// set width 120px for input with attribute name started by 'my'
+// console.log checked checkbox
+// show all images with a cat
+
+// wrap every image into the div
+// for .mbox with index 3 set padding-top 50px
+// for first div wraper for img set float left and border red
