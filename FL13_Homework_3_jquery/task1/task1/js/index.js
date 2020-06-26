9d94d86fb4af948bf1e7c8486ab75e804e9db824
Viktor Marchuk <dayverr87@gmail.com>
2020-06-26 16:33:59 +0300

    ADD: task1

--- FL13_Homework_3_jquery/task1/task1/js/index.js
+++ FL13_Homework_3_jquery/task1/task1/js/index.js
@@ -1,25 +1,33 @@
 // Example: $('#someId').css({'prop' : 'value', 'prop1' : 'value1', ...});
 
 //body: add padding 30px
+$('body').css({'padding' : '30px'})
 
 // id title: make align center, remove top margin
+$('#title').css({'textAlign':'center','marginTop':'0px'})
 
 // div after header: add double border and padding 20px
+$('header').next().css({'border':'3px double black', 'padding':'20px'})
 
 // change color for all h2 and set top margin to 0
+$('h2').css({'color':'blue', 'marginTop':'0px'})
+
 
 // set font size 18px for all .list direct children
-
+$('.list').children().css({'fontSize':'18px'})
 
 // in #list-1
   // show all hidden and not cloned li
-
-  // hide empty li
+$('#list-1 li:hidden').not('.cloned').show()
+// hide empty li
+$('#list-1 li:empty').hide()
 
 // in #list-3
   // for all even li set margin-left -20px
+  $('#list-3 li:odd').css({'marginLeft':'20px'})
 
   // for the first li set any different color
+  
 
   // for all li with index > 5 set color to #ccc
 
