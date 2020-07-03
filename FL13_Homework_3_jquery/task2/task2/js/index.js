53631dbfdf88a45cbfd8848cae46e9309a610621
Viktor Marchuk <dayverr87@gmail.com>
2020-07-03 16:51:51 +0300

    FIX: t2

--- FL13_Homework_3_jquery/task2/task2/js/index.js
+++ FL13_Homework_3_jquery/task2/task2/js/index.js
@@ -13,3 +13,30 @@ const todos = [
     done: true
   }
 ];
+
+let todosCopy = [...todos];
+
+
+
+function addItemToDom (textOfTodos, isDone){
+  if(isDone==false){
+    $list.append(`<li class="item"><span class="item-text">${textOfTodos}</span><button class="item-remove">Remove</button></li>`)
+  } else {
+  $list.append(`<li class="item"><span class="item-text done">${textOfTodos}</span><button class="item-remove">Remove</button></li>`)
+  }
+}
+
+$list.empty();
+todosCopy.forEach(element => {
+  addItemToDom(element.text,element.done)
+});
+
+$add.click(function(e){
+  e.preventDefault();
+  todosCopy.push({text: $input.val(), done: false});
+  $list.empty();
+  todosCopy.forEach(element => {
+  addItemToDom(element.text,element.done)
+  })
+})
+
